55554eaac31d493bedf023d6d48077a3316474a6
joha7809 <joha7809@gmail.com>
2025-10-27 16:20:09 +0100

    Added decoding function

--- isa-encoder/src/encoder.rs
+++ isa-encoder/src/encoder.rs
@@ -1,5 +1,13 @@
-use isa_core::{codec::EncodeError, traits::Encodable, types::ResolvedInstruction};
+use isa_core::{
+    codec::EncodeError,
+    traits::{Decodable, Encodable},
+    types::ResolvedInstruction,
+};
 
 pub fn encode_program(program: &[ResolvedInstruction]) -> Result<Vec<u32>, EncodeError> {
     program.iter().map(|i| i.encode()).collect()
+}
+
+pub fn decode_program(program: &[u32]) -> Result<Vec<ResolvedInstruction>, EncodeError> {
+    program.iter().map(|i| i.decode()).collect()
 }
